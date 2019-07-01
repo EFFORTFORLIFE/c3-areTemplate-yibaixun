fd2c975b2f56a13ae18e70e7496d1e8578754af7
linxs <1475311472@qq.com>
2019-07-01 12:12:56 +0800

    add

---
+++ yibaixun/php/customerdata.php
@@ -0,0 +1,3 @@
+<?php
+      echo '[{"pic":"imgs/indexImgs/brand.jpg","t":"风雨同行"},{"pic":"imgs/indexImgs/brand.jpg","t":"风雨同行"},{"pic":"imgs/indexImgs/brand.jpg","t":"风雨同行"},{"pic":"imgs/indexImgs/brand.jpg","t":"风雨同行"}]';
+?>
---
+++ yibaixun/php/indexdata.php
@@ -0,0 +1,7 @@
+<?php
+    $achievement = $_GET["dataname"];
+    if($achievement=="achievement"){
+       // echo '[{"num":10,"pvalue":"10年高端网站设计经验"},{"num":10,"pvalue":"10年高端网站设计经验"},{"num":10,"pvalue":"10年高端网站设计经验"}]';
+        echo '[{"num":10,"pvalue":"10年高端网站设计经验"},{"num":"28+","pvalue":"28+政府单位的认可"},{"num":"56+","pvalue":"56+上市公司的选择"},{"num":"800+","pvalue":"800+品牌客户的选择"}]';
+    }
+?>
